4c7362a9f62bb7399c56760cf779efb480857248
Lorenzo Stipani <lorenzo.stipani.ext@orange.com>
2025-01-17 18:19:12 +0100

    kickoff with required files

--- client_multithreading/client.py
+++ client_multithreading/client.py
@@ -1,51 +1,23 @@
-import os
 import json
-import logging
-import numpy as np
-import threading
-import asyncio
-from typing import List
+import multithreading
 from websockets.sync.client import connect
 
-logging.basicConfig(
-    format="%(message)s",
-    level=os.getenv("LOGLEVEL", "INFO"),
-)
+from commons.logger import logger
+from commons.parser import get_URI
+from commons.broker import Bus
 
 
-async def receiver(URI: str, data: List[int]) -> None:
+def main(bus: Bus) -> None:
     try:
-        async with connect(URI) as websocket:
-            async for message in websocket:
-                data.append(json.loads(message)["value"])
-                await asyncio.sleep(0)
+        with connect(get_URI()) as websocket:
+            for message in websocket:
+                bus.add(json.loads(message)["value"])
+                logger.info(bus)
     except ConnectionRefusedError as error:
-        logging.error(error)
+        logger.error(error)
         raise
-
-
-def calculator(data: List[int]) -> None:
-    arr = np.array(data)
-    logging.info("mean: %.6f and std: %.6f", np.mean(arr), np.std(arr))
-
-
-def main(data: List[int]):
-    try:
-        URI = f"ws://{os.environ["WS_HOST"]}:{os.environ["WS_PORT"]}/ws"
-    except KeyError:
-        logging.error("missing WS_HOST and/or WS_PORT as env vars")
-        raise
-
-    threads = [
-        asyncio.to_thread(receiver, URI, data),
-        threading.Thread(target=calculator, args=(data,)),
-    ]
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()
 
 
 if __name__ == "__main__":
-    data: List[int] = []
-    main(data)
+    bus = Bus.factory(data=[0])
+    main(bus)
